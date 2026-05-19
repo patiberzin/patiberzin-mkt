f68fd60409dc248c1d8772dc6728b7b7cba05f78
patiberzin <patiberzin@gmail.com>
2026-05-19 20:29:19 -0300

    inicio

--- blog/astro.config.mjs
+++
@@ -1,8 +0,0 @@
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-import { defineConfig } from 'astro/config';
-
-export default defineConfig({
-  site: 'http://localhost:4321',
-  base: '/patiberzin-mkt/',
-});
